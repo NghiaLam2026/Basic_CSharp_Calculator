109f14ba11c35e1032b4afd6d47aea81cea4811c
Nghia Lam <118234173+NghiaLam2026@users.noreply.github.com>
2024-08-31 13:06:28 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Transactions;
+
+int first_number;
+int second_number;
+
+Console.WriteLine("Welcome to my basic calculator!");
+Console.Write("Enter the first number:" );
+first_number = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter the second number:" );
+second_number = int.Parse(Console.ReadLine());
+
+string option;
+Console.WriteLine("Enter +, -, *, or /");
+option = Console.ReadLine();
+
+if (option == "+")
+{
+    Console.WriteLine("The solution is {0}", first_number + second_number);
+} else if (option == "-")
+{
+    Console.WriteLine("The solution is {0}", first_number - second_number);
+} else if (option == "*")
+{
+    Console.WriteLine("The solution is {0}", first_number - second_number);
+} else if (option == "/")
+{
+    Console.WriteLine("The solution is {0}", first_number / second_number);
+}
